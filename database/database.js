770ff0c908322008d9bbcd742ae1ffe15db6a067
Fuckner <100722316+Fuckners@users.noreply.github.com>
2022-06-27 14:56:17 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const sequelize = require('sequelize');
 
-const connection = new sequelize('apigames', 'root', '@Banco_Dados', {
+const connection = new sequelize('apigames', 'root', '123456', {
     host: 'localhost',
     dialect: 'mysql',
     timezone: '-03:00'
